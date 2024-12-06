7d6a5e396b1b812728cecf03a8935830debb1139
BFStReEt <toidxbp02@gmail.com>
2024-12-06 14:51:34 +0700

    second commitm

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Realtime Chat App || BF</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <section class="form signup">
+            <form action="#" method="post" enctype="multipart/form-data" autocomplete="off">
+                .
+            </form>
+        </section>
+    </div>
+
+</body>
+
+</html>
